3d757bef3e08f7b9cec613ef0752772bbae96f8b
daniel-caso-github <daniel.caso.quintanilla@gmail.com>
2025-12-10 18:52:02 -0500

    chore: initialize Go modules with core dependencies

---
+++ cmd/api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	log.Info().Msg("🚀 Starting Real-Time Alerting System...")
+
+	// TODO: Cargar configuración
+	// TODO: Inicializar base de datos
+	// TODO: Inicializar servidor HTTP
+	// TODO: Inicializar WebSocket
+
+	log.Info().Msg("Server started successfully")
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Info().Msg("Shutting down server...")
+}
